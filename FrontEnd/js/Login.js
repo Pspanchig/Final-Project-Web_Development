e369d452b345b0d800a2e9f27ac2797cef4c184d
Pablo-Panchig <pspanchig@gmail.com>
2023-11-30 02:34:07 -0700

    Code

--- FrontEnd/js/Login.js
+++ FrontEnd/js/Login.js
@@ -4,38 +4,31 @@ async function loginUser() {
     const isProfessorChecked = document.getElementById('loginAsOption1').checked;
 
     const APIURL = 'http://localhost:5006/OtherPath';
-    try {
+    
         const response = await fetch(APIURL);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
         const users = await response.json();
-
         const user = users.find(u => u.name === username);
 
-        if (user && user.password === password) {
-            const userData = {
-                username: user.name,
-                isProfessor: user.IsProfessor
-            };
-            localStorage.setItem('currentUser', JSON.stringify(userData));
+    if (user && user.password === password) {
+        const userData = {
+            username: user.name,
+            isProfessor: user.IsProfessor
+        };
+        localStorage.setItem('currentUser', JSON.stringify(userData));
 
-            if (isProfessorChecked && user.IsProfessor) {
-                alert("Login as Professor Successfully");
-                window.location.href = 'professor-page.html'; 
-            } else if (!isProfessorChecked && !user.IsProfessor) {
-                alert("Login Successfully");
-                window.location.href = '/FrontEnd/Page3.html';
-            } else {
-                alert("Role does not match with our records.");
-            }
+        if (isProfessorChecked && user.isProffesor) {
+            alert("Login as Professor Successfully");
+            window.location.href = 'ProfessorPage1.html'; 
+        } else if (!isProfessorChecked && !user.IsProfessor) {
+            alert("Login Successfully");
+            window.location.href = '/FrontEnd/Page3.html';
         } else {
-            alert("Invalid username or password.");
+            alert("Role does not match with our records.");
         }
-    } catch (error) {
-        console.error('Error during login:', error);
-        alert("An error occurred while trying to log in.");
+    } else {
+        alert("Invalid username or password.");
     }
+    
 }
 
 
